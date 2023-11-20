fa181acda668a746836c94a0ca522ce599ded1e5
Rublevska <73138546+Rublevska@users.noreply.github.com>
2023-11-20 22:03:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    key: Date.now(),
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page ||
+      prevState.key !== this.state.key
+    ) {
+      // ОТРЕЗАТЬ ID ЗАПРОСА ИЗ QUERY
+      // делаем http запрос с query и page
+      // записываем результат в images
+    }
+  }
+
+  handleSubmit = newQuery => {
+    this.setState({
+      query: `${newQuery}`,
+      page: 1,
+      images: [],
+      key: Date.now(),
+    });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}></form>
+
+        <div>Gallery</div>
+
+        <button onClick={this.handleLoadMore}>Load more</button>
+      </div>
+    );
+  }
+}
